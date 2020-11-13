fafcdd923dc879f2a20d46cf7df39636b7c96996
github-Lilya <1656725121@qq.com>
2020-11-13 11:22:50 +0800

    测试推送2

--- src/main/java/com/rong/Test1.java
+++ src/main/java/com/rong/Test1.java
@@ -1,9 +1,12 @@
 package com.rong;
 
-public class Test {
+public class Test1 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
         System.out.println("第一次输出");
+
+
+
     }
 
 }
