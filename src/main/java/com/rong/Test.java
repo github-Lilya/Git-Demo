37739856a14bcd47a8e89f26769b2689b05f1576
github-Lilya <1656725121@qq.com>
2020-11-13 11:32:08 +0800

    测试推送4

--- src/main/java/com/rong/Test.java
+++ src/main/java/com/rong/Test.java
@@ -4,6 +4,8 @@ public class Test {
 
     public static void main(String[] args) {
         System.out.println("第一次输出");
+
+        System.out.println("test的第二次输出");
     }
 
 }
--- src/main/java/com/rong/Test1.java
+++ src/main/java/com/rong/Test1.java
@@ -5,6 +5,8 @@ public class Test1 {
     public static void main(String[] args){
         System.out.println("第一次输出");
 
+        System.out.println("test1的第二次输出");
+
 
 
     }
